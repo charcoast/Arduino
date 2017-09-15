dbc52801f0b83d5cce0fc14ae418ae2131b786a8
Augusto Oliveira <augusto.o@aluno.ifsc.edu.br>
2017-09-15 12:30:11 -0300

    Update Ultrassonico.ino

--- Ultrassonico.ino
+++ Ultrassonico.ino
@@ -7,12 +7,12 @@
 
 #include <Ultrasonic.h> //Inclusão da biblioteca
 
-#define LEDVerde 5    //Define LEDVerde
-#define LEDVermelho 6 //Define LEDVermelho
-#define buzzer 7      //Define buzzer
+#define LEDVerde 5    //Define o pino LEDVerde
+#define LEDVermelho 6 //Define o pino LEDVermelho
+#define buzzer 7      //Define o pino buzzer
 
-#define Echo 10       //Define Echo
-#define Trigger 11    //Define Trigger
+#define Echo 10       //Define o pino Echo
+#define Trigger 11    //Define o pino Trigger
 Ultrasonic ultrasonic(Trigger, Echo); //Inicia o sensor ultrassonico usando os valores de Trigger(11) e Echo (10)
 void setup() {
   pinMode(buzzer, OUTPUT);  //Define buzzer como sendo um pino OUTPUT
@@ -26,7 +26,7 @@ void setup() {
 void loop() {
   float distancia = ultrasonic.distanceRead();  //Armazena a leitura da distância na variável flutuante distancia
 
-  if (distancia <= 2.0) { //Condicional com distancia
+  if (distancia == 2.0) { //Condicional com distancia
     digitalWrite(LEDVerde, HIGH); //Definindo nível lógico alto ao LEDVerde
     digitalWrite(LEDVermelho, LOW); //Definindo nível lógico baixo ao LERVermelho
     Serial.print("Distancia: ");  //Escreve no monitor serial
@@ -34,10 +34,12 @@ void loop() {
     Serial.println("Porta fechada");  //Escreve no monitor serial
     tone(buzzer, 850);  //Toca no buzzer com frequência 850
     delay(200); //Delay de 200ms
+    noTone(buzzer);
+    delay(100);
     tone(buzzer, 850);
     delay(200);
     noTone(buzzer); //Para de tocar no buzzer
-  } else {
+  } else if(distancia > 2.0) {
     digitalWrite(LEDVerde, LOW);  //Definindo nível lógico baixo ao LEDVerde
     digitalWrite(LEDVermelho, HIGH);  //Definindo nível lógico alto ao LEDVermelho
     Serial.print("Distancia: ");  //Escreve no monitor serial
@@ -45,6 +47,8 @@ void loop() {
     Serial.println("Porta aberta"); //Escreve no monitor serial
     tone(buzzer, 300);  //Toca no buzzer com frequência 300
     delay(200); //Delay de 200ms
+    noTone(buzzer);
+    delay(100);
     tone(buzzer, 300);
     delay(200);
     noTone(buzzer); //Para de tocar no buzzer
